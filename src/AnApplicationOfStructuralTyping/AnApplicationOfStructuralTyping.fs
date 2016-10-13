0d13d4f211a11f188eb351ac87fcd8a80a8ea830
Raphael Schweizer <code@caringdev.ch>
2016-10-13 16:13:38 +0200

    An application of structural typing

---
+++ src/AnApplicationOfStructuralTyping/AnApplicationOfStructuralTyping.fs
@@ -0,0 +1,50 @@
+﻿module AnApplicationOfStructuralTyping
+
+    open System
+
+    open FSharp.Data
+
+    type XmlMessage = XmlProvider<"messageSamples.xml", SampleIsList=true, Global=true>
+
+    type DbMessage = {
+        Available : string
+        Missing : string
+        MissingNil : Nullable<int>
+        Nil : Nullable<decimal>
+    }
+
+    let map (xmlMessage : XmlMessage.Message) =
+        let missingNil =
+            xmlMessage.MissingNil
+            |> Option.bind (fun n -> match n.Nil with Some true -> None | _ -> Some n.Value)
+            |> Option.toNullable
+        let nil =
+            match xmlMessage.Nil.Nil with Some true -> None | _ -> Some xmlMessage.Nil.Value
+            |> Option.toNullable
+        {
+            Available = xmlMessage.Available
+            Missing = xmlMessage.Missing |> Option.toObj
+            MissingNil = missingNil
+            Nil = nil
+        }
+
+    type Option<'T> with
+        static member inline ofNillable n =
+            match (^N : (member Nil : bool option) n) with
+            | Some true -> None
+            | _ -> Some (^N : (member Value : 'T) n)
+
+    type Nillable =
+        static member inline toNullable n =
+            Option.ofNillable n |> Option.toNullable
+        static member inline toNullable n =
+            Option.bind Option.ofNillable n |> Option.toNullable
+
+
+    let map' (xmlMessage : XmlMessage.Message) =
+        {
+            Available = xmlMessage.Available
+            Missing = xmlMessage.Missing |> Option.toObj
+            MissingNil = xmlMessage.MissingNil |> Nillable.toNullable
+            Nil = xmlMessage.Nil |> Nillable.toNullable
+        }
